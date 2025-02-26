6a7bed109c0da5921cf9f858048dee6c1408071b
abhishek-T99 <timilsina.abhishek55@gmail.com>
2025-02-26 13:31:54 +0545

    feat: Created Register user API view

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from userauths import views as userauths_views
 
 urlpatterns = [
     path("user/token/", userauths_views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("user/register/", userauths_views.RegisterView.as_view(), name="auth_register"),
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,5 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from userauths.models import Profile, User
@@ -6,3 +8,10 @@ from userauths.serializers import MyTokenObtainPairSerializer, RegisterSerialize
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    serializer_class = RegisterSerializer
