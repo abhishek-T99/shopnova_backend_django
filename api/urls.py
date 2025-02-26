e1f3d4c80392a7d7a98ae8f418bcae3e2f08363e
abhishek-T99 <timilsina.abhishek55@gmail.com>
2025-02-26 16:06:34 +0545

    feat: Added token refresh view

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from userauths import views as userauths_views
 
 urlpatterns = [
     path("user/token/", userauths_views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("user/token/refresh/", TokenRefreshView.as_view(), name="token"),
     path("user/register/", userauths_views.RegisterView.as_view(), name="auth_register"),
 ]
